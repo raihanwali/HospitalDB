8c8be49a47c7f6e24cf7355898e59c96c0901881
partha marella <marellapartha7c@gmail.com>
2017-04-06 11:11:02 +0530

    Add files via upload
    all the entries are here

---
+++ all_entries.sql
@@ -0,0 +1,316 @@
+
+\c hospitaldb
+
+insert into patient values(
+     '123456789000',
+           'Arankar',
+            23,
+            1234567890,
+         '#1 8th block Nagarbhavi flat 221B'
+);
+
+insert into patient values(
+         '134567890023',
+         'Brankar',
+         45,
+         1234567891,
+         '#2 3rd block Nagarbhavi flat 22B'
+);
+
+insert into patient values(
+         '123456789046',
+         'Partha',
+         18,
+         1234567878,
+         '#3 7th block Vijayanagar flat 402b'
+);
+
+insert into patient values(
+         '123456789067',
+         'Raihan',
+         23,
+         1234567854,
+         '#4 1st block Vijayanagar flat 56c'
+);
+
+insert into patient values(
+         '123456789088',
+         'Kiru',
+         65,
+         1234567711,
+         '#5 7th block MGroad flat 781c'
+);
+
+insert into patient values(
+         '123456789128',
+         'Mirana',
+         23,
+         1234567342,
+         '#4 3rd block psych towers flat 69c'
+);
+
+insert into patient values(
+         '123456789116',
+         'Carl',
+         30,
+         1234567342,
+         '#6 14th block Greenwoods flat 781c'
+);
+
+insert into patient values(
+         '123456756123',
+         'Bhargav',
+         25,
+         1246100100,
+         '#21 2nd block JPnagar flat 231'
+);
+
+insert into patient values(
+         '123456754577',
+         'Ananth',
+         12,
+         1235457689,
+         '#34 3rd cross, Nagarbavi, Shoba apts, flat 201'
+);
+insert into patient values(
+         '123456745687',
+         'Rohit',
+         32,
+         1393454576,
+         '#9 2nd cross, Majestic'
+);
+
+insert into patient values(
+         '123456745463',
+         'Rajiv',
+         20,
+         1393456467,
+         '#10 Gadhi road, Shamshabad '
+);
+
+insert into patient values(
+         '123456987562',
+         'Ananya',
+         39,
+         1947581203,
+         'Flat no 2, adiya residency, Kukatpally'
+);
+
+insert into patient values(
+         '102985746389',
+         'SUraj',
+         13,
+         1839285781,
+         '#42 Sanjeev Apts, Hebbal'
+);
+
+insert into patient values(
+         '144473829847',
+         'Pandey.P',
+         34,
+         1039484756,
+         '#7 DHoni Villas, Yashwantpura'
+);
+
+insert into patient values(
+         '174859838498',
+         'SUjata',
+         39,
+         1888575848,
+         '#10 Dhruva Medows, flat 1A, JP nagar'
+);
+
+insert into patient values(
+         '198723458769',
+         'Kaveri',
+         29,
+         1938746589,
+         '#9 Lavang Nagar,Karthika nagara'
+);
+/*-----------------------patient--------*/
+insert into employee values('Dr.Vishal',1,50,'1960-02-12','#1 8th block Nagarbhavi flat 221B',1243456576,'Senior oncologist','Full time');
+insert into employee values('Dr.Bhuvaneshwar',2,40,'1970-01-26','#3 7th block Vijayanagar flat 402b',1473859285,'Senior Heart Surgeon','Full time');
+insert into employee values('Dr.Aditya',3,45,'1965-05-9','#4 1st block Vijayanagar flat 56c',1385937485,'Senior Neurologist','Full time');
+insert into employee values('Dr.Bhatra',4,42,'1964-08-24','#5 7th block MGroad flat 781c',1209857684,'Senior Orthopedist','Part time');
+insert into employee values('Dr.Dravid',5,40,'1970-09-28','#4 3rd block psych towers flat 69c',1295748020,'Senior Dentist','Full time');
+insert into employee values('Dr.Zaheer',6,39,'1971-02-19','#6 14th block Greenwoods flat 781c',1000023458,'Senior Endocrinologist.','Full time');
+insert into employee values('Dr.Reddy',7,33,'1978-06-17','#21 2nd block JPnagar flat 231',1229348579,'Senior Dermatologist','Part time');
+insert into employee values('.Ramya',8,30,'1980-11-26','#34 3rd cross, Nagarbavi, Shoba apts, flat 201',1829391283,'Head Nurse','Full time');
+insert into employee values('Saraswati',9,29,'1985-05-14','#9 2nd cross, Majestic',1399928374,'Nurse','Full time');
+insert into employee values('Parvati',10,37,'1975-11-24','#10 Gadhi road, Shamshabad ',1120394855,'Nurse','Full time');
+insert into employee values('Swetha',11,37,'1978-07-26','Flat no 2, adiya residency, Kukatpally',1928394857,'Nurse','Full time');
+insert into employee values('Sharma',12,35,'1982-02-14','#42 Sanjeev Apts, Hebbal',1203957688,'Lab technician','Full time');
+insert into employee values('Raghu',13,32,'1980-01-30','#7 DHoni Villas, Yashwantpura',1002394857,'Lab technician','Part time');
+insert into employee values('Dr.Pingle',14,33,'1979-10-15','#10 Dhruva Medows, flat 1A, JP nagar',1201924839,'Senior oncologist','Full time');
+insert into employee values('Dr.Nithin',15,38,'1973-11-20','#9 Lavang Nagar,Karthika nagara',1664728394,'Internal Medicine Specialist','Full time');
+insert into employee values('Dr.Bharati',16,40,'1971-12-23','villa-2, aditya edenwoods, Tellapur',1884759839,'Senior  Gynecologist','Part time');
+                                                                /*------------employee--------*/
+
+        insert into room values(101,'type-1: A/C room',4500,'Vacant','Personal nurse, Personal chef, TV ,visiting room privilegdes..');
+        insert into room values(102,'type-1: A/C room',4500,'Occupied','Personal nurse, Personal chef, TV ,visiting room privilegdes..');
+        insert into room values(103,'type-1: A/C room',4500,'Vacant','Personal nurse, Personal chef, TV ,visiting room privilegdes..');
+        insert into room values(104,'type-1: A/C room',4500,'Occupied','Personal nurse, Personal chef, TV ,visiting room privilegdes..');
+        insert into room values(201,'type-1: A/C room',4500,'Vacant','Personal nurse, Personal chef, TV ,visiting room privilegdes.');
+        insert into room values(202,'type-1: A/C room',4500,'Occupied','Personal nurse, Personal chef, TV ,visiting room privilegdes.');
+        insert into room values(203,'type-2: A/C room',3500,'Vacant','TV, visiting room privilegdes.');
+        insert into room values(204,'type-2: A/C room',3500,'Occupied','TV, visiting room privilegdes.');
+        insert into room values(301,'type-2: A/C room',3500,'Vacant','TV, visiting room privilegdes.');
+        insert into room values(302,'type-2:  A/C room',3500,'Vacant','TV, visiting room privilegdes.');
+        insert into room values(303,'type-2: A/C room',4500,'Vacant','TV, visiting room privilegdes.');
+        insert into room values(304,'type-1: A/C room',4500,'Vacant','TV, visiting room privilegdes.');
+        insert into room values(01,'type-1: A/C room',4500,'Vacant','TV, visiting room privilegdes.');
+
+                                                    /*---------room---------*/
+                                                    insert into report values(
+                                                                          0051,
+                                                                          '123456754577',
+                                                                          'Ananth',
+                                                                          3,
+                                                                          '2017-02-24',
+                                                                          250,
+                                                                          0011);
+                                                    insert into report values(
+                                                                          0057,
+                                                                          '123456745687',
+                                                                          'Rohit',
+                                                                          5,
+                                                                          '2017-02-27',
+                                                                          550,
+                                                                          0012);
+                                                    insert into report values(
+                                                                          0021,
+                                                                          '123456745463',
+                                                                          'Rajiv',
+                                                                          1,
+                                                                          '2017-03-12',
+                                                                          550,
+                                                                          0013);
+                                                    insert into report values(
+                                                                          0051,
+                                                                          '123456987562',
+                                                                          'Ananya',
+                                                                          4,
+                                                                          '2017-03-15',
+                                                                          250,
+                                                                          0014);
+                                                    insert into report values(
+                                                                          0021,
+                                                                          '102985746389',
+                                                                          'SUraj',
+                                                                          7,
+                                                                          '2017-03-09',
+                                                                          300,
+                                                                          0015);
+                                                    insert into report values(
+                                                                          0057,
+                                                                          '144473829847',
+                                                                          'Pandey.P',
+                                                                          6,
+                                                                          '2017-03-20',
+                                                                          600,
+                                                                          0016);
+                                                    insert into report values(
+                                                                            0057,
+                                                                            '174859838498',
+                                                                            'SUjata',
+                                                                            5,
+                                                                            '2017-03-17',
+                                                                            800,
+                                                                            0017);
+                                                    insert into report values(
+                                                                            0051,
+                                                                            '198723458769',
+                                                                            'Kaveri',
+                                                                            7,
+                                                                            '2017-03-21',
+                                                                            150,
+                                                                            0018);
+/*----------------report--------*/
+insert into med_history values('123456789000','8388','5408686','241','140','170','Arankar');
+insert into med_history values('134567890023','7654','4198764','291','173','140','Brankar');
+insert into med_history values('123456789046','6543','4298762','210','192','119','Partha');
+insert into med_history values('123456789067','9875','4913256','271','110','153','Raihan');
+insert into med_history values('123456789088','5987','4032142','190','94','167','Kiru');
+insert into med_history values('123456789128','8754','4013567','170','103','182','Mirana');
+insert into med_history values('123456789116','8210','5109723','287','119','119','Carl');
+insert into med_history values('123456756123','9843','8715511','219','139','103','Bhargav');
+insert into med_history values('123456754577','9831','6154314','203','153','94','Ananth');
+insert into med_history values('123456745687','6201','5514144','301','109','150','Rohit');
+insert into med_history values('123456745463','7341','4363553','318','99','173','Rajiv');
+insert into med_history values('123456987562','7934','4515353','298','135','153','Ananya');
+insert into med_history values('102985746389','7631','4816235','190','169','172','SUraj');
+insert into med_history values('144473829847','9211','5973636','217','158','101','Pandey.P');
+insert into med_history values('174859838498','4982','4749151','271','201','120','SUjata');
+insert into med_history values('198723458769','4721','5176801','205','198','126','Kaveri');
+/*--------------med_history-----*/
+insert into lab values(
+     'Biochemistry',
+           0051,
+            12,
+            1234567890
+);
+insert into lab values(
+     'Haematology',
+           0057,
+            13,
+            1234567890
+);
+insert into lab values(
+     'X-ray and other analysis',
+           0021,
+            15,
+            1234567890
+);
+/*---------------lab------------*/
+insert into in_patient values('123456754577','2017-02-24','2017-02-28',102,'Ananth',1234567545,1244700769);
+insert into in_patient values('123456745687','2017-02-27','2017-03-01',103,'Rohit',1234567456,1244706221);
+insert into in_patient values('123456745463','2017-03-01','2017-03-04',101,'Rajiv',1234567454,1244707987);
+insert into in_patient values('123456987562','2017-03-03','2017-03-06',104,'Ananya',1234569875,1244701546);
+insert into in_patient values('102985746389','2017-03-05','2017-03-06',201,'SUraj',1029857463,1244705173);
+insert into in_patient values('144473829847','2017-03-02','2017-03-05',204,'Pandey.P',1444738298,1244704326);
+insert into in_patient values('174859838498','2017-03-11','2017-03-14',201,'SUjata',1748598384,1224705173);
+insert into in_patient values('198723458769','2017-03-12','2017-03-15',304,'Kaveri',1987234587,1244701410);
+
+    /*--------------in_patient--------*/
+
+    insert into out_patient values('123456789000','9','2017-02-24','Arankar','1');
+    insert into out_patient values('134567890023','10','2017-02-27','Brankar','1');
+    insert into out_patient values('123456789046','9','2017-03-01','Partha','2');
+    insert into out_patient values('123456789067','9','2017-03-03','Raihan','4');
+    insert into out_patient values('123456789088','10','2017-03-05','Kiru','5');
+    insert into out_patient values('123456789128','11','2017-03-02','Mirana','3');
+    insert into out_patient values('123456789116','10','2017-03-11','Carl','4');
+    insert into out_patient values('123456756123','11','2017-03-12','Bhargav','6');
+
+/*-------------op_bill------------------*/
+
+insert into out_patient_bill values(500,'123456789000',0000010,0,500,0,'2017-02-24','Arankar');
+insert into out_patient_bill values(500,'134567890023',0000011,0,800,0,'2017-02-25','Brankar');
+insert into out_patient_bill values(750,'123456789046',0000012,0,300,0,'2017-02-28','Partha');
+insert into out_patient_bill values(800,'123456789067',0000013,0,250,0,'2017-03-04','Raihan');
+insert into out_patient_bill values(600,'123456789088',0000014,0,550,0,'2017-03-08','Kiru');
+insert into out_patient_bill values(550,'123456789128',0000015,0,900,0,'2017-03-11','Mirana');
+insert into out_patient_bill values(800,'123456789116',0000016,0,100,0,'2017-03-17','Carl');
+insert into out_patient_bill values(780,'123456756123',0000017,0,230,0,'2017-03-13','Bhargav');
+/* ----------------in_patient_bill----------------*/
+insert into in_patient_bill values('123456754577','Ananth',0,500,4500,250,'2017-02-28',102,600,0);
+insert into in_patient_bill values('123456745687','Rohit',0,500,4500,550,'2017-03-01',103,1000,0);
+insert into in_patient_bill values('123456745463','Rajiv',0,750,4500,550,'2017-03-04',101,750,0);
+insert into in_patient_bill values('123456987562','Ananya',0,750,4500,250,'2017-03-03',104,750,0);
+insert into in_patient_bill values('102985746389','SUraj',0,650,4500,300,'2017-03-06',201,750,0);
+insert into in_patient_bill values('144473829847','Pandey.P',0,600,3500,600,'2017-03-05',204,800,0);
+insert into in_patient_bill values('174859838498','SUjata',0,800,4500,800,'2017-03-1',202,775,0);
+insert into in_patient_bill values('198723458769','Kaveri',0,800,4500,150,'2017-03-15',304,750,0);
+/*------------queries------------*/
+select * from out_patient_bill;
+update out_patient_bill set VAT=(doc_fee+med_charge)*0.05;
+    select * from out_patient_bill;
+update out_patient_bill set total=(doc_fee+med_charge+VAT);
+    select * from out_patient_bill;
+update in_patient_bill  set VAT=(doc_fee+room_charge+med_charge+report_cost)*.05;
+    select * from in_patient_bill;
+update in_patient_bill  set total=doc_fee+room_charge+med_charge+report_cost+VAT;
+    select * from in_patient_bill;
+select count(*) from in_patient_bill where total>5000;
+select * from out_patient_bill where VAT> 50;
+select name from med_history where bp>130 and sugar>130;
